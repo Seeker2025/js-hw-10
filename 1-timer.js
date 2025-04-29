3cb00b0e1ed9175936e9f5faf3924c3c6184adbd
Seeker2025 <85400723+Seeker2025@users.noreply.github.com>
2025-04-29 15:08:07 +0000

    Deploying to gh-pages from @ Seeker2025/js-hw-10@7d7736bb2a1b02b6ad9ee888fee224c051dcbf0c 🚀

--- 1-timer.js
+++ 1-timer.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */console.log("Timer module loaded");let e=5;const o=setInterval(()=>{e-=1,console.log(e),e===0&&(clearInterval(o),console.log("Timer stopped"))},1e3);
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import"./assets/vendor-DnzlHthD.js";console.log("Timer module loaded");let e=5;const o=setInterval(()=>{e-=1,console.log(e),e===0&&(clearInterval(o),console.log("Timer stopped"))},1e3);
 //# sourceMappingURL=1-timer.js.map
