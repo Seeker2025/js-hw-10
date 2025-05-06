117336c46806d92796be9bcb50c463c77e479f5e
Seeker2025 <85400723+Seeker2025@users.noreply.github.com>
2025-05-06 09:32:20 +0000

    Deploying to gh-pages from @ Seeker2025/js-hw-10@eb664f5f6431c9559ad2f69751a6a7e37f90837b 🚀

--- 2-snackbar.js
+++ 2-snackbar.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */console.log("Snackbar module loaded");
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */console.log("Snackbar module loaded");let o=null;const l={name:"Nata03",funObj(){o=this.name}};l.funObj();console.log(o);
 //# sourceMappingURL=2-snackbar.js.map
