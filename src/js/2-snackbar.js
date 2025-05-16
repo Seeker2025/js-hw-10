db1efdd4ba2a25fd4a0adfd4e88a48b8930cbcc0
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2025-05-16 14:27:34 +0300

    go on

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,14 +1,45 @@
 console.log('Snackbar module loaded');
 
-let a = null;
-const obj01 = {
-    name: 'Nata03',
-    funObj(){
-        // console.log(this.name);
-        a = this.name;
-        
-    }
+const formRef = document.querySelector('form.form');
+// console.log(formRef);
+
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event){
+    event.preventDefault();
+    // console.log(event.currentTarget.elements.delay.value);
+    // console.log(event.currentTarget.elements.state.value);
+    const isObj = {};
+
+    const data = new FormData(event.currentTarget);
+    data.forEach((value, name)=>{
+        isObj[name]=value;
+     });
+    console.log(isObj);
+    const { delay, state } = isObj;
+    toQuickPromise({ delay, state });
 };
 
-obj01.funObj();
-console.log(a);
+function toQuickPromise({ delay, state }){
+    setTimeout(()=>{
+    new Promise((resp, rej)=>{
+        // let num = Math.random();
+        if(state==='fulfilled') resp('Fulfilled');
+        else rej('Rejected');
+    }).then((resp)=>console.log(resp))
+    .catch((rej)=>console.log(rej))    
+
+    }, delay);
+}
+// toQuickPromise('pee');
+// console.log('a'==='a');
+
+
+
+function toNum(w){
+    if(w==='a') console.log('a');
+    if(w==='b') console.log('b');
+}
+// toNum('a');
+
+// console.log('a'==='a');
