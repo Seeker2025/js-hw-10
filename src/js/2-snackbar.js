c532da2e0e1b09cea6ca021e248c66e7d5240337
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2025-05-17 11:02:35 +0300

    go on

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,5 +1,10 @@
 console.log('Snackbar module loaded');
 
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
+
 const formRef = document.querySelector('form.form');
 // console.log(formRef);
 
@@ -9,6 +14,7 @@ function onFormSubmit(event){
     event.preventDefault();
     // console.log(event.currentTarget.elements.delay.value);
     // console.log(event.currentTarget.elements.state.value);
+    const form = event.target;
     const isObj = {};
 
     const data = new FormData(event.currentTarget);
@@ -16,24 +22,56 @@ function onFormSubmit(event){
         isObj[name]=value;
      });
     console.log(isObj);
+     console.log(typeof(isObj.delay));
     const { delay, state } = isObj;
     toQuickPromise({ delay, state });
+    form.reset();
 };
 
 function toQuickPromise({ delay, state }){
+    const num = Number(delay);
+    // console.log(typeof(num));
     setTimeout(()=>{
-    new Promise((resp, rej)=>{
+         new Promise((resp, rej)=>{
         // let num = Math.random();
         if(state==='fulfilled') resp('Fulfilled');
         else rej('Rejected');
-    }).then((resp)=>console.log(resp))
-    .catch((rej)=>console.log(rej))    
+    }).then((resp)=>{
+        console.log(resp)
+        iziToast.show({
+                        position: 'topRight', // bottomRight, bottomLeft, topRight
+                        backgroundColor: 'DarkGreen',
+                        titleColor: 'white',
+                        messageColor: 'white',
+                                  // theme: 'light', //light
+                                  // color: 'green', // blue, red, green, yellow
+                        title: 'Success!',
+                        message: `Fulfilled promise in ${num}ms`
 
+                              });
+    })
+    .catch((rej)=>{
+        iziToast.show({
+                        position: 'topRight', // bottomRight, bottomLeft, topRight
+                        backgroundColor: 'darkRed',
+                        titleColor: 'white',
+                        messageColor: 'white',
+                                  // theme: 'light', //light
+                                  // color: 'green', // blue, red, green, yellow
+                        title: 'Fail!',
+                        message: `Rejected promise in ${num}ms`
+
+
+                              });
+
+        console.log(rej);
+    }).finally(()=>{
+
+    })
     }, delay);
 }
 // toQuickPromise('pee');
 // console.log('a'==='a');
-
 
 
 function toNum(w){
